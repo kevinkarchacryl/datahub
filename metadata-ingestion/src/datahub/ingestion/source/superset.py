6f2f855e4e88bb309fc86ef4dc7bb22f87f0a0b9
Kevin Karch <kevin.karch@acryl.io>
2025-02-26 13:56:30 -0500

    feat(ingestion): fix doc description

--- metadata-ingestion/src/datahub/ingestion/source/superset.py
+++ metadata-ingestion/src/datahub/ingestion/source/superset.py
@@ -148,7 +148,7 @@ class SupersetConfig(
     )
     database_pattern: Optional[AllowDenyPattern] = Field(
         default=None,
-        description="Regex patterns for databases to filter in ingestion.",
+        description="Regex patterns for databases to filter out datasets in ingestion.",
     )
     username: Optional[str] = Field(default=None, description="Superset username.")
     password: Optional[str] = Field(default=None, description="Superset password.")
